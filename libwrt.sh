95590282b2954829a18ddd9f91fa0ce337f7165a
ing <1123745939@qq.com>
2025-12-30 15:00:07 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -5,7 +5,6 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 
 rm -rf package/emortal/luci-app-athena-led
 rm -rf package/luci/applications/luci-app-lucky
-rm -rf package/luci/applications/luci-app-oaf
 
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
